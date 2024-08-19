c972766e6cba9fba31c49f22a903498f14fa3be7
DreadfulThrumbo <harmondwayne58@gmail.com>
2024-08-19 05:29:49 -0400

    Create function to get the random computer choice

--- javascript.js
+++ javascript.js
@@ -2,4 +2,24 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-console.log(getRandomInt(3))
+function getComputerChoice() {
+    let randomInt = getRandomInt(3);
+    let choice = "hi"
+
+    switch (randomInt) {
+        case 0:
+            choice = "rock";
+            break;
+        case 1:
+            choice = "paper";
+            break;
+        case 2:
+            choice = "scissors";
+            break;
+        default:
+            choice = "face punch";
+            break;
+    }
+    return choice;
+}
+
